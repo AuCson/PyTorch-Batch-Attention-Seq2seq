7f4ef8b31ccf616ee99cac2914770a67881435fe
AuCson <2474010692@qq.com>
2017-09-04 22:27:59 +0800

    first commit

---
+++ attentionRNN.py
@@ -0,0 +1,118 @@
+import torch
+from torch import nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+
+class EncoderRNN(nn.Module):
+    def __init__(self, input_size, embed_size, hidden_size, n_layers=1, dropout=0.5):
+        super(EncoderRNN, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.embed_size = embed_size
+        self.n_layers = n_layers
+        self.dropout = dropout
+        self.embedding = nn.Embedding(input_size,embed_size)
+        self.gru = nn.GRU(embed_size, hidden_size, n_layers, dropout=self.dropout, bidirectional=True)
+
+    def forward(self, input_seqs, input_lengths, hidden=None):
+        '''
+        :param input_seqs: 
+            Variable of shape (num_step(T),batch_size(B)), sorted decreasingly by lengths(for packing)
+        :param input:
+            list of sequence length
+        :param hidden:
+            initial state of GRU
+        :returns:
+            GRU outputs in shape (T,B,hidden_size(H))
+            last hidden stat of RNN(i.e. last output for GRU)
+        '''
+        embedded = self.embedding(input_seqs)
+        packed = torch.nn.utils.rnn.pack_padded_sequence(embedded, input_lengths)
+        outputs, hidden = self.gru(packed, hidden)
+        outputs, output_lengths = torch.nn.utils.rnn.pad_packed_sequence(outputs)  # unpack (back to padded)
+        outputs = outputs[:, :, :self.hidden_size] + outputs[:, :, self.hidden_size:]  # Sum bidirectional outputs
+        return outputs, hidden
+
+
+class Attn(nn.Module):
+    def __init__(self, method, hidden_size):
+        super(Attn, self).__init__()
+        self.method = method
+        self.hidden_size = hidden_size
+        self.attn = nn.Linear(self.hidden_size * 2, hidden_size)
+        self.v = nn.Parameter(torch.rand(hidden_size))
+        self.softmax = nn.Softmax()
+
+    def forward(self, hidden, encoder_outputs):
+        '''
+        :param hidden: 
+            previous hidden state of the decoder, in shape (layers*directions,B,H)
+        :param encoder_outputs:
+            encoder outputs from Encoder, in shape (T,B,H)
+        :return
+            attention energies in shape (B,T)
+        '''
+        max_len = encoder_outputs.size(0)
+        this_batch_size = encoder_outputs.size(1)
+        # For storing attention energies
+        attn_energies = Variable(torch.zeros(this_batch_size, max_len))
+        if self.config.USE_CUDA:
+            attn_energies = attn_energies.cuda()
+        H = hidden.repeat(max_len,1,1).transpose(0,1)
+        encoder_outputs = encoder_outputs.transpose(0,1) # [B*T*H]
+        attn_energies = self.score(H,encoder_outputs) # compute attention score
+        return self.softmax(attn_energies).unsqueeze(1) # normalize with softmax
+
+    def score(self, hidden, encoder_outputs):
+        energy = self.attn(torch.cat([hidden, encoder_outputs], 2)) # [B*T*2H]->[B*T*H]
+        energy = energy.transpose(2,1) # [B*H*T]
+        v = self.v.repeat(encoder_outputs.data.shape[0],1).unsqueeze(1) #[B*1*H]
+        energy = torch.bmm(v,energy) # [B*1*T]
+        return energy.squeeze(1) #[B*T]
+
+class BahdanauAttnDecoderRNN(nn.Module):
+    def __init__(self, hidden_size, embed_size, output_size, n_layers=1, dropout_p=0.1):
+        super(BahdanauAttnDecoderRNN, self).__init__()
+        # Define parameters
+        self.hidden_size = hidden_size
+        self.embed_size = embed_size
+        self.output_size = output_size
+        self.n_layers = n_layers
+        self.dropout_p = dropout_p
+        # Define layers
+        self.embedding = nn.Embedding(output_size, embed_size)
+        self.dropout = nn.Dropout(dropout_p)
+        self.attn = Attn('concat', hidden_size)
+        self.gru = nn.GRU(hidden_size + embed_size, hidden_size, n_layers, dropout=dropout_p)
+        #self.attn_combine = nn.Linear(hidden_size + embed_size, hidden_size)
+        self.out = nn.Linear(hidden_size * 2, output_size)
+
+    def forward(self, word_input, last_hidden, encoder_outputs):
+        '''
+        :param word_input:
+            word input for current time step, in shape (B)
+        :param last_hidden:
+            last hidden stat of the decoder, in shape (layers*direction*B*H)
+        :param encoder_outputs:
+            encoder outputs in shape (T*B*H)
+        :return
+            decoder output
+        Note: we run this one step at a time i.e. you should use a outer loop 
+            to process the whole sequence
+        '''
+        # Get the embedding of the current input word (last output word)
+        word_embedded = self.embedding(word_input).view(1, word_input.data.shape[0], -1) # (1,B,N)
+        word_embedded = self.dropout(word_embedded)
+        # Calculate attention weights and apply to encoder outputs
+        attn_weights = self.attn(last_hidden[-1], encoder_outputs)
+        context = attn_weights.bmm(encoder_outputs.transpose(0, 1))  # (B,1,N)
+        context = context.transpose(0, 1)  # (1,B,N)
+        # Combine embedded input word and attended context, run through RNN
+        rnn_input = torch.cat((word_embedded, context), 2)
+        #rnn_input = self.attn_combine(rnn_input) # use it in case your size of rnn_input is different
+        output, hidden = self.gru(rnn_input, last_hidden)
+        output = output.squeeze(0)  # (1,B,N)->(B,N)
+        context = context.squeeze(0)
+        output = F.log_softmax(self.out(torch.cat((output, context), 1)))
+        # Return final output, hidden state
+        return output, hidden
